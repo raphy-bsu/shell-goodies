af0d99ae162bb0b039a36cc08c452a579e3c3f5b
Oleg Orlov <orelcokolov@gmail.com>
2016-08-19 16:23:13 +0300

    Remove pull_request function

--- script/git.bash
+++ script/git.bash
@@ -13,14 +13,6 @@ function current_issue(){
   echo `current_branch` | awk -F_ '{print $1}' | grep -o '[0-9]\+'
 }
 
-function pull_request(){
-  if [[ -n $(current_issue) ]]; then
-    hub pull-request -i `current_issue`
-  else
-    echo "Can not fetch issue number, make sure you are on valid branch"
-  fi
-}
-
 function gp(){
   echo "PUSH to $(current_branch)"
   git push origin `current_branch`
